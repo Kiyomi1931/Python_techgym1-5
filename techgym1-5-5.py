85df8ad0602154c4f7f1baa6f40859636ae39e7f
Kiyomi1931 <71973734+Kiyomi1931@users.noreply.github.com>
2022-02-17 23:57:17 +0900

    Python1-5 5回目解答
    動作確認済みです。

--- techgym1-5-5.py
+++ techgym1-5-5.py
@@ -7,7 +7,14 @@ def start_message():
 
 def get_my_hand():
   print('自分の手を入力してください')
-  return int(input('0:グー, 1:チョキ, 2:パー'))
+  input_message=''
+  index=0
+  for hand in hands:
+    input_message+=str(index)+':'+hand
+    if index < 2:
+      input_message+=','
+    index+=1
+  return int(input(input_message))
 
 def get_you_hand():
   return random.randint(0, 2)
